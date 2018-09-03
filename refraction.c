ddd7ab615c90c78d113e115fae66a44d901c7995
RavenKottke <42457901+RavenKottke@users.noreply.github.com>
2018-09-03 13:23:25 -0500

    Add files via upload

--- refraction.c
+++ refraction.c
@@ -1,1 +1,11 @@
+/*
+  * A program that measures the angle of refraction as light enters a medium
+  *
+  */
 
+
+
+
+  printf("Angle of incidence: %lf degrees\n", );
+  printf("Index of refraction: %lf\n", );
+  printf("Angle of refraction: %lf degrees\n", );
